da304a8d4b9fca720a2fc552fa6f89a967e7b6d7
Sergio Wilson <sergio.wilson-galvez@orion.com>
2023-10-30 09:32:27 -0500

    refactor method

--- projects/lazymodule/src/lib/services/bootsstrap.service.ts
+++ projects/lazymodule/src/lib/services/bootsstrap.service.ts
@@ -12,26 +12,24 @@ export class BootstrapService {
     switch (key) {
       case LazyLoadBroker.Financial: {
         const { DashboardFinancialsModule } = await import('@mods/dashboards/financials/dashboard-financials.module');
-        this.loadComponent(DashboardFinancialsModule, container);
+        this.createBootstrapComponent(DashboardFinancialsModule, container);
 
         break;
       }
       case LazyLoadBroker.MarketSummary: {
         const { DashboardMarketSummaryModule } = await import('@mods/dashboards/market-summary/dashboard-market-summary.module');
-        this.loadComponent(DashboardMarketSummaryModule, container);
+        this.createBootstrapComponent(DashboardMarketSummaryModule, container);
         break;
       }
 
     }
   }
 
-  loadComponent<TModule>(ngModule: NgModuleOfType<TModule>, container: ViewContainerRef) {
+  createBootstrapComponent<TModule>(ngModule: NgModuleOfType<TModule>, container: ViewContainerRef) {
     const moduleRef = createNgModule(ngModule, this.injector);
     const component = AsTypeOf<IAsLazyLoadedModule>(moduleRef.instance).boostrapComponent();
     container.clear();
     container.createComponent(component, { ngModuleRef: moduleRef });
   }
-
-
 
 }
